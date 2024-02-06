60755f019432a86c2fe7764c6201474f527ade72
Medtex <medtexprog@gmail.com>
2024-02-06 12:58:10 +0100

    newD02

---
+++ OBSHR/src/components/common/HouseTypeSelector.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HouseTypeSelector = () => {
+
+    const [hous]
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default HouseTypeSelector
--- OBSHR/src/components/house/AddHouse.jsx
+++ OBSHR/src/components/house/AddHouse.jsx
@@ -1,45 +1,120 @@
 import React, {useState} from 'react'
+import {addHouse} from "../utils/ApiFunctions.js";
+import {preview} from "vite";
 
 const AddHouse = () => {
-  const [newHouse, setNewHouse] = useState({
-    photo :null,
-    houseType: "",
-    housePrice: "",
-    numOfRoom: "",
-    numOfBathroom: ""
-  })
+    const [newHouse, setNewHouse] = useState({
+        photo: null,
+        houseType: "",
+        housePrice: "",
+        numOfRoom: "",
+        numOfBathroom: ""
+    })
 
-  const [imagePreview, setImagePreview] = useState("")
-  const [successMessage, setSuccessMessage] =useState("")
-  const [erroMessage, setErrorMessage]= useState("")
+    const [imagePreview, setImagePreview] = useState("")
+    const [successMessage, setSuccessMessage] = useState("")
+    const [erroMessage, setErrorMessage] = useState("")
 
 
-  const handleRoomInputChange = (e) => {
-    const name = e.target.name
-    let value = e.target.value
-    if (name ===  "housePrice") {
-      if (!isNaN(value)) {
-        value.parseInt(value)
-      } else {
-        value = ""
-      }
+    const handleRoomInputChange = (e) => {
+        const name = e.target.name
+        let value = e.target.value
+        if (name === "housePrice") {
+            if (!isNaN(value)) {
+                value.parseInt(value)
+            } else {
+                value = ""
+            }
+        }
+        setNewHouse({...newHouse, [name]: value})
     }
-    setNewHouse({...newHouse,[name]:value})
-  }
 
-  const handleImageChange = (e) => {
-    const selectedImage = e.target.files[0]
-    setNewHouse({...newHouse, photo: selectedImage})
-    setImagePreview(URL.createObjectURL(selectedImage))
-  }
+    const handleImageChange = (e) => {
+        const selectedImage = e.target.files[0]
+        setNewHouse({...newHouse, photo: selectedImage})
+        setImagePreview(URL.createObjectURL(selectedImage))
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const success = await addHouse(newHouse.photo, newHouse.houseType, newHouse.housePrice, newHouse.numOfRoom, newHouse.numOfBathroom)
+
+            if (success !== undefined) {
+                setSuccessMessage("a new house was added to the database")
+                setNewHouse({photo: null, houseType: "", housePrice: "", numOfBathroom: "", numOfRoom: ""})
+                setImagePreview("")
+                setErrorMessage("")
+            } else {
+                setErrorMessage("Error adding house")
+            }
+        } catch (error) {
+            setErrorMessage(error.message)
+        }
+    }
 
 
+    return (
+        <>
+            <section className="container, mt-5 mb-5">
+                <div className="row justify-content-center">
+                    <div className="col-mb-8 col-lg-6">
+                        <h2 className="mt-5 mb-2"> Add a New House </h2>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label htmlFor="houseType" className="form-label"> House Type
+                                </label>
+                                <div>
+
+                                </div>
+
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="housePrice" className="form-label"> House Price
+                                </label>
+                                <input className="form-control"
+                                       required
+                                       id="housePrice"
+                                       name="housePrice"
+                                       type="number"
+                                       value={newHouse.housePrice}
+                                       onChange={handleRoomInputChange}/>
 
 
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="photo" className="form-label"> House Photo
+                                </label>
+                                <input
+                                    id="photo"
+                                    name="photo"
+                                    type="file"
+                                    className="form-control"
+                                    value={newHouse.photo}
+                                    onChange={handleImageChange}/>
 
-  return (
-    <div>AddHouse</div>
-  )
+                                {imagePreview && (
+                                    <img src={imagePreview}
+                                         alt="preview House Photo"
+                                         style={{maxWidth: "400px", maxHeight: "400px"}}
+                                         className="mb-3"
+                                    />
+                                )}
+
+                            </div>
+                            <div className="d-grid d-flex mt-2">
+                                <button className="btn btn-outline-primary ml-5">
+                                    save House
+                                </button>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
 }
 
 export default AddHouse
